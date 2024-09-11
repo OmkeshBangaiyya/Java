01fd81046ce252236babc7aeb4b25e69f9c7e83b
Bangaiyya Omkesh Dnyaneshwar <88362326+OmkeshBangaiyya@users.noreply.github.com>
2024-09-11 13:02:56 +0530

    Loops in java

---
+++ forLoop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner
+
+public class forLoop {
+    public static void main(String[] args) {
+       
+        // print 100 time hello
+
+        for(int counter=0 ; counter<100 ; counter=counter+1 ){
+        System.out.println("Hello");
+        }
+
+        // print number from 0 to 10
+        // a++ => a+1
+        for(int a =0 ; a<=10 ; a++){
+        System.out.println(a);
+        }
+    }
+
+}
---
+++ whileLoop.java
@@ -0,0 +1,27 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        // int i=0;
+        // while (i<11) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+
+        //do-while loop runs at least one time because it check the condition at last 
+        // int b = 0;
+        // do{
+        //     System.out.println(b);
+        //     b++;
+        // }while(b>=11);
+
+        //sum of n natural number n=4
+        int n =0;
+        while (n<=4) {
+            System.out.println(n);
+            n = n*(n+1)/2; 
+        }
+    }
+    
+}
+
+
